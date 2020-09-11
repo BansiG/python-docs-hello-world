f5665fa1c9f847aee106f5ac241295d46ec5fc56
BansiG <bansi.gajera28@gmail.com>
2020-09-11 06:15:25 +0000

    modified application.py

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = False
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "Hello !!!"
+
+@app.route("/api", methods=['GET'])
+def initialize():
+    CompanyID = int(request.args.get('companyID'))
+    BranchID = int(request.args.get('branchID'))
+    return "sucessssssss"
+
+@app.errorhandler(404) 
+def not_found(e): 
+  return "404 Not Found"
+
+
+if __name__ == "__main__":
+    app.run() 
