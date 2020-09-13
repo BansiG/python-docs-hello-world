c00ddf22948ae20fad5cdc19a13a75292868191a
BansiG <bansi.gajera28@gmail.com>
2020-09-13 11:42:38 +0000

    added getModel in app

--- db2.py
+++ db2.py
@@ -31,6 +31,49 @@ def connect(companyID=None, branchID=None):
 
 
 def getModel(companyID=None, branchID=None):
+    result="True"
+    print("*********************************result-----------")
+    try:
+        # DB = {'servername': 'tcp:server-india.database.windows.net,1433', 'database': 'MyDatabase', 'username': 'bansi@server', 'password': 'Azure@123'}
+        # # conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=scan-n-go-server.database.windows.net,1433', user='sqluser@server', password='Azure@123', database='ScanNGoDB')
+        # conn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server='
+        #                       + DB['servername']
+        #                       + ';Database=' + DB['database']
+        #                       + ';Uid=' + DB['username']
+        #                       + ';Pwd=' + DB['password']
+        #                       )
+        drivers = [item for item in pyodbc.drivers()]
+        driver = drivers[-1]
+        print("driver:{}".format(driver))
+        server = 'tcp:server-india.database.windows.net,1433'
+        database = 'MyDatabase'
+        uid = 'bansi'
+        pwd = 'Azure@123'
+        con_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}'
+        conn = pyodbc.connect(con_string)
+        cursor = conn.cursor()
+        result = "connnected"
+        sql = """SELECT * from Records where (CompanyID = ? AND BranchID = ?)"""
+        cursor.execute(sql, (companyID,branchID))
+        records = cursor.fetchall()
+
+        for row in records:
+            cid= row[0]
+            bid = row[1]
+            date = row[2]
+            ver = row[3]
+            print("company id is", cid)
+            print("branch id is", bid)
+            print("version is ", ver)
+            print("date is ", date)
+
+        result="Version" + str(ver) + " released on Date" +str(date)
+    except Exception as ex:
+        result = str(ex)
+    return result
+
+"""
+def getModel(companyID=None, branchID=None):
 	  try:
         drivers = [item for item in pyodbc.drivers()]
         driver = drivers[-1]
@@ -42,8 +85,8 @@ def getModel(companyID=None, branchID=None):
         con_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}'
         conn = pyodbc.connect(con_string)
         cursor = conn.cursor()
-    
-        sql = """SELECT * from Records where (CompanyID = ? AND BranchID = ?)"""
+
+        sql = SELECT * from Records where (CompanyID = ? AND BranchID = ?)
         cursor.execute(sql, (companyID,branchID))
         records = cursor.fetchall()
 
@@ -71,3 +114,4 @@ def getModel(companyID=None, branchID=None):
 		return result
 
 
+"""
